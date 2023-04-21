68a0b61d1d7598e0bb68deae3718b581eff8be3f
gacloudi <113767409+gacloudi@users.noreply.github.com>
2023-04-21 19:50:01 +0530

    Update SA.py

--- SA.py
+++ SA.py
@@ -14,7 +14,7 @@ c1,c2=st.columns(2)
 conn = sqlite3.connect('response.db', check_same_thread=False)
 c = conn.cursor()
 c.execute('CREATE TABLE IF NOT EXISTS response (NPS number,Star number, Feature Text,Completion Time)')
-c.commit()
+conn.commit()
 
 with c1:
     NPS=st.slider("1.How likely would you be to recommend our products to a friend or colleague?",1,10,1)
