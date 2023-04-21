a28b717a00e100b080eddd0c3f193ed88b0d7d36
gacloudi <113767409+gacloudi@users.noreply.github.com>
2023-04-21 19:46:04 +0530

    Update SAA.py

--- SAA.py
+++ SAA.py
@@ -17,7 +17,7 @@ st.write(wd)
 #q="SELECT * FROM '{}'".format(str(db_dir))
 db_dir='response'
 #q="SELECT * FROM '{}'".format(str(db_dir))
-q="SELECT * FROM response"
+q="SELECT * FROM 'response'"
 st.write(q)
 c.execute(q)
 data = c.fetchall()
