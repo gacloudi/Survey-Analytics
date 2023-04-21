9ee73bccde62bdbfc45726cd71f65e2143a4daa4
gacloudi <113767409+gacloudi@users.noreply.github.com>
2023-04-21 18:36:04 +0530

    Update SA.py

--- SA.py
+++ SA.py
@@ -36,8 +36,8 @@ wd=getcwd()
 st.write(wd)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-db_dir = (BASE_DIR + '/response.db')
-
+#db_dir = (BASE_DIR + '/response.db')
+db_dir='response'
 q="SELECT * FROM '{}'".format(str(db_dir))
 c.execute(q)
 data = c.fetchall()
